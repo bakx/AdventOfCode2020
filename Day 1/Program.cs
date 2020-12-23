89b49dd8f56c38aff76c2ecc175db495a2b83090
Gideon Bakx <gideon@bakx.ca>
2020-12-23 00:58:54 -0500

    Completed day 1

---
+++ Day 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Read entries
+            StreamReader reader = new StreamReader("input.txt");
+            int[] items = reader.ReadToEnd().Split(Environment.NewLine).Select(int.Parse).ToArray();
+            reader.Close();
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                for (int j = 0; j < items.Count(); j++)
+                {
+                    if (items[i] + items[j] != 2020)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"The answer is {items[i]} * {items[j]} = {items[i] * items[j]} ");
+                    break;
+                }
+            }
+        }
+    }
+}
