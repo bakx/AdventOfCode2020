70b19a49da34b3f6d5607861518f1d8d88b2ca72
Gideon Bakx <gideon@bakx.ca>
2020-12-23 23:54:24 -0500

    Added day 3 and 4

---
+++ Day 3/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Read entries
+            StreamReader reader = new StreamReader("input.txt");
+
+            List<ForestLine> lines = reader
+                .ReadToEnd()
+                .Split(Environment.NewLine)
+                .Select(e => new ForestLine(e))
+                .ToList();
+
+            reader.Close();
+
+            List<Directions> directions = new List<Directions>()
+            {
+                new Directions {X = 1, Y = 1},
+                new Directions {X = 3, Y = 1},
+                new Directions {X = 5, Y = 1},
+                new Directions {X = 7, Y = 1},
+                new Directions {X = 1, Y = 2}
+            };
+
+
+            foreach (Directions direction in directions)
+            {
+                int directionX = 1;
+
+                Debug.WriteLine($"Starting with direction : X {direction.X} Y: {direction.Y}");
+
+                for (int index = 0; index < lines.Count; index += direction.Y)
+                {
+                    ForestLine line = lines[index];
+
+                    if (index > 0)
+                    {
+                        directionX += direction.X;
+                    }
+
+                    int checkPosition;
+                    if (directionX > line.Line.Length)
+                    {
+                        // Recalculate the position, taking the endless duplication of the field in the same pattern
+                        // into account
+                        checkPosition = directionX % line.Line.Length;
+                        if (checkPosition == 0)
+                        {
+                            checkPosition = line.Line.Length;
+                        }
+                    }
+                    else
+                    {
+                        checkPosition = directionX;
+                    }
+
+                    // Next check if 0 based index.
+                    checkPosition--;
+
+                    Debug.WriteLine($"X: {checkPosition + 1} Y: {index + 1} Character: {line.Line[checkPosition]}");
+
+                    if (line.Line[checkPosition] == '#')
+                    {
+                        direction.TreeCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Final results:");
+
+            foreach (Directions direction in directions)
+            {
+                Console.WriteLine($"X: {direction.X} Y: {direction.Y} : Tree's encountered {direction.TreeCount}");
+            }
+
+            int totalTrees = directions[0].TreeCount;
+            for (int i = 1; i < directions.Count; i++)
+            {
+                totalTrees *= directions[i].TreeCount;
+            }
+
+            Console.WriteLine($"Answer: {totalTrees}");
+        }
+    }
+
+    public class ForestLine
+    {
+        public ForestLine(string line)
+        {
+            Line = line;
+        }
+
+        public string Line { get; set; }
+    }
+
+    public class Directions
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int TreeCount { get; set; }
+    }
+}
---
+++ Day 4/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day4
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Read entries
+            StreamReader reader = new StreamReader("input.txt");
+
+            List<Passport> passports = new List<Passport>();
+
+            StringBuilder builder = new StringBuilder();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    // Process passport
+                    passports.Add(new Passport(builder.ToString()));
+                    builder.Clear();
+                }
+
+                builder.AppendLine(line);
+            }
+
+            // Process final passport
+            passports.Add(new Passport(builder.ToString()));
+
+
+            // Get valid passports
+            Console.WriteLine($"Part #1: Valid passports {passports.Count(p => p.HasAllFieldsPart1)}");
+            Console.WriteLine($"Part #2: Valid passports {passports.Count(p => p.HasAllFieldsPart2)}");
+        }
+    }
+
+    public class Passport
+    {
+        public Passport(string blob)
+        {
+            string[] data = blob.Replace(Environment.NewLine, " ").Split(' ');
+
+            foreach (string item in data)
+            {
+                if (item.StartsWith("ecl"))
+                {
+                    EyeColor = item.Split(':')[1];
+                }
+
+                if (item.StartsWith("pid"))
+                {
+                    PassportId = item.Split(':')[1];
+                }
+
+                if (item.StartsWith("eyr"))
+                {
+                    ExpirationYear = Convert.ToInt32(item.Split(':')[1]);
+                }
+
+                if (item.StartsWith("hcl"))
+                {
+                    HairColor = item.Split(':')[1];
+                }
+
+                if (item.StartsWith("byr"))
+                {
+                    BirthYear = Convert.ToInt32(item.Split(':')[1]);
+                }
+
+                if (item.StartsWith("iyr"))
+                {
+                    IssueYear = Convert.ToInt32(item.Split(':')[1]);
+                }
+
+                if (item.StartsWith("cid"))
+                {
+                    CountryId = Convert.ToInt32(item.Split(':')[1]);
+                }
+
+                if (item.StartsWith("hgt"))
+                {
+                    Height = item.Split(':')[1];
+                }
+            }
+        }
+
+        public bool HasAllFieldsPart1 =>
+            BirthYear != default &&
+            IssueYear != default &&
+            ExpirationYear != default &&
+            PassportId != default &&
+            Height != default &&
+            EyeColor != default &&
+            HairColor != default;
+
+        public bool HasAllFieldsPart2 =>
+            ValidBirthYear() &&
+            ValidIssueYear() &&
+            ValidExpirationYear() &&
+            ValidPassportId() &&
+            ValidEyeColor() &&
+            ValidHairColor() &&
+            ValidHeight();
+
+        private bool ValidBirthYear()
+        {
+            return BirthYear != default && BirthYear >= 1920 && BirthYear <= 2002;
+        }
+
+        private bool ValidIssueYear()
+        {
+            return IssueYear != default && IssueYear >= 2010 && IssueYear <= 2020;
+        }
+
+        private bool ValidExpirationYear()
+        {
+            return ExpirationYear != default && ExpirationYear >= 2020 && ExpirationYear <= 2030;
+        }
+
+        private bool ValidPassportId()
+        {
+            return PassportId != default && PassportId.Length == 9 && Regex.IsMatch(PassportId, "[0-9]{9}");
+        }
+
+        private bool ValidEyeColor()
+        {
+            return EyeColor != default && EyeColor == "amb" || EyeColor == "blu" ||
+                   EyeColor == "brn" || EyeColor == "gry" ||
+                   EyeColor == "grn" || EyeColor == "hzl" || EyeColor == "oth";
+        }
+
+        private bool ValidHairColor()
+        {
+            return HairColor != default && HairColor.Length == 7 && Regex.IsMatch(HairColor, "^#[0-9a-f]{6}");
+        }
+
+        public bool ValidHeight()
+        {
+            if (Height == default)
+            {
+                return false;
+            }
+
+            // Unable to parse
+            if (!int.TryParse(Height.Replace("cm", "").Replace("in", ""), out int parsedHeight))
+            {
+                return false;
+            }
+
+            if (Height.EndsWith("cm"))
+            {
+                return parsedHeight >= 150 && parsedHeight <= 193;
+            }
+
+            if (Height.EndsWith("in"))
+            {
+                return parsedHeight >= 59 && parsedHeight <= 76;
+            }
+
+            return false;
+        }
+
+        public int BirthYear { get; set; }
+        public int IssueYear { get; set; }
+        public int ExpirationYear { get; set; }
+        public string PassportId { get; set; }
+        public int CountryId { get; set; }
+        public string Height { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+    }
+}
